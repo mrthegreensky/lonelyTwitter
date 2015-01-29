7fbf525ea7c049dff4fd5de5f9b3882cc6c11ab0
mrthegreensky <azhong@ualberta.ca>
2015-01-29 14:29:03 -0700

    Added LonelyTweetModelTest

---
+++ LonelyTwitterTestsTest/src/ca/ualberta/cs/lonelytwitter/test/LonelyTweetModelTest.java
@@ -0,0 +1,17 @@
+package ca.ualberta.cs.lonelytwitter.test;
+
+import ca.ualberta.cs.lonelytwitter.LonelyTwitterActivity;
+import android.test.ActivityInstrumentationTestCase2;
+
+public class LonelyTweetModelTest extends
+		ActivityInstrumentationTestCase2<LonelyTwitterActivity> {
+
+	public LonelyTweetModelTest(String name) {
+		super(LonelyTwitterActivity.class);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+}
